9e064d8532a55d68005488eba0ef3700f964b592
Emelia Wilkinson <emelia.wilkinson@datastax.com>
2023-12-05 14:01:39 -0500

    serverless instruction updates

---
+++ src/main/java/ConnectDatabase.java
@@ -0,0 +1,27 @@
+import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.core.cql.ResultSet;
+import com.datastax.oss.driver.api.core.cql.Row;
+import java.nio.file.Paths;
+
+public class ConnectDatabase {
+
+   public static void main(String[] args) {
+       // Create the CqlSession object:
+       try (CqlSession session = CqlSession.builder()
+           .withCloudSecureConnectBundle(Paths.get("/Users/emelia.wilkinson/Driver Testing/java/secure-connect-nonvectortestdb.zip"))
+           .withAuthCredentials("DAuJHbTyApKuLRuqgroinZZF","cNSum6tHX+17Ygcpyy_Bvo70B-ncxCscqWaywtx+XGF3J-Hy0dEi+h9iQtEmmJD8GQsekaKy7RHkx.gwJlTZsuMxYt-FsU.KD3OvE0Z5G-OhQS7CZH.U.xDMBs_LGhSR")
+           .withKeyspace("non_vector")
+           .build()) {
+           // Select the release_version from the system.local table:
+           ResultSet rs = session.execute("select release_version from system.local");
+           Row row = rs.one();
+           //Print the results of the CQL query to the console:
+           if (row != null) {
+               System.out.println(row.getString("release_version"));
+           } else {
+               System.out.println("An error occurred.");
+           }
+       }
+       System.exit(0);
+   }
+}
